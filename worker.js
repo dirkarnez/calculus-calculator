379bdbc0fd42d3a0053557cbd6fd492982fb323b
Dirk Arnez <smalldirkalex@gmail.com>
2023-12-27 20:23:25 +0800

    - add worker.js

---
+++ worker.js
@@ -0,0 +1,21 @@
+importScripts("https://cdn.jsdelivr.net/pyodide/v0.24.1/full/pyodide.js);
+   loadPyodide({
+      fullStdLib: true,
+      stdout: msg => console.log(`${msg}`)//consoleOutput.innerHTML += `${msg}<br>`,
+    })
+    .then(_pyodide => {
+      console.log(`just now ${!!pyodide ? "overriding..." : "new"}`);
+      pyodide = _pyodide;
+    });
+// onmessage = function(e) {
+  
+              
+//   const result = e.data[0] * e.data[1];
+//   if (isNaN(result)) {
+//     postMessage('Please write two numbers');
+//   } else {
+//     const workerResult = 'Result: ' + result;
+//     console.log('Worker: Posting message back to main script');
+//     postMessage(workerResult);
+//   }
+// }
